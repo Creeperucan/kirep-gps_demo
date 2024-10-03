2804281985ac30d3d1cc644f909f3a6e4fbd895a
Creeperucan <67125206+Creeperucan@users.noreply.github.com>
2024-10-03 20:47:31 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {
+    General = {
+        updateInterval = 1000,          -- 1000 = 1 second (It is not recommended to set it to 0.)
+        playerBlip = true,              -- If the value is "true" the player's own blip remains closed, but if the value is "false" the player's blip remains open. (Coming Soon)
+        itemName = 'gps',               -- Item Name
+    },
+}
+
+-- For Blip Information: https://docs.fivem.net/docs/game-references/blips/
---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'on'
+
+name 'kirep-gps_demo'
+author 'Creeperucan'
+description 'Demo Version'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+ui_page 'html/index.html'
+
+escrow_ignore {
+    'config.lua',
+  }
+
+files {
+
+  -- HTML
+    'html/index.html',
+    'html/style.css',
+    'html/app.js',
+}
+dependency '/assetpacks'
